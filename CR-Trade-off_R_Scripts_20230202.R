efa9d06712e20aaa58c3c965b71bcc0149b04b10
jinnyyang <42893792+jinnyyang@users.noreply.github.com>
2023-02-02 19:20:26 -0500

    Add files via upload

--- CR-Trade-off_R_Scripts_20230202.R
+++ CR-Trade-off_R_Scripts_20230202.R
@@ -2,10 +2,8 @@
 #Jinny Wu Yang, Feng-Hsun Chang, Yi-Chun Yeh, An-Yi Tsai, Fuh-Kwo Shiah, Kuo-Ping Chiang, Gwo-Ching Gong, Chih-hao Hsieh
 #jinnyang@umich.edu
 source("I-C_Rcode_Functions.R")
-
-######### Part 1: Calculate net growth rate for each ASV########
-library("gdata")
-library("phyloseq")
+library(gdata)
+library(phyloseq)
 library(ggpubr)
 library(stats)
 library(gdata)
@@ -13,6 +11,11 @@ library(lavaan)
 library(lme4)
 library(nlme)
 library(MASS)
+library(RADanalysis)
+library(reshape2)
+
+######### Part 1: Calculate net growth rate and for each ASV########
+##### A. Per-capita net growth rate (PNGR) ~ top-down control dilution factors (TCDF) relationship #########
 
 RawTable=read.table("Final_table_dada2.tsv",header=T, row.names=1) #Input raw ASV table
 SampleID=read.xls("SampleName.xlsx", sheet=1,header=F) #Input sample name list
@@ -73,43 +76,39 @@ write.csv(NetGrowthRate_D30, quote=F,file = paste("NetGrowthRate_D30",
 
 }
 
+##### B. Extract competition ability (intercetp) and resistance to top-down control (regression slope) from PBGR~TCDF relationsihp #########
+#Input sample list 
+table_list = list.files(pattern="Net*.*csv") 
+s = strsplit(table_list, "\\.")
+CruiseStation=sapply(s,"[",2)
+ss = strsplit(sapply(s,"[",1), "\\_")
+Treatment=sapply(ss,"[",2)
 
-#################################### End of Part 1 ########################################
+for (i in 1:length(table_list)){
+  NetGrowth=read.csv(table_list[i],header=T,row.names = 1) #Input 
+  is.na(NetGrowth)=sapply(NetGrowth, is.infinite)
+  NetGrowth[is.na(NetGrowth)]=0
   
-######### Part 2: Calculate regression slope (resistance) and intercept (top-down control-free growth) ####
   
-  #Input sample list 
-  table_list = list.files(pattern="Net*.*csv") 
-  s = strsplit(table_list, "\\.")
-  CruiseStation=sapply(s,"[",2)
-  ss = strsplit(sapply(s,"[",1), "\\_")
-  Treatment=sapply(ss,"[",2)
-
-  for (i in 1:length(table_list)){
-    NetGrowth=read.csv(table_list[i],header=T,row.names = 1) #Input 
-    is.na(NetGrowth)=sapply(NetGrowth, is.infinite)
-    NetGrowth[is.na(NetGrowth)]=0
-    
-    
-    Dilution=sapply(strsplit(colnames(NetGrowth), "D"),"[",3)
-
-    EachASV=matrix(0,nrow(NetGrowth),3)
-    for (j in 1:nrow(NetGrowth)){
-      l=lm(as.numeric(NetGrowth[j,])~as.numeric(Dilution))
+  Dilution=sapply(strsplit(colnames(NetGrowth), "D"),"[",3)
+  
+  EachASV=matrix(0,nrow(NetGrowth),3)
+  for (j in 1:nrow(NetGrowth)){
+    l=lm(as.numeric(NetGrowth[j,])~as.numeric(Dilution))
     EachASV[j,1]=coef(l)[2] #Slope
     EachASV[j,2]=summary(l)$coefficients[2,4] #p-value
     EachASV[j,3]=coef(l)[1] #intercept
-    }
+  }
   rownames(EachASV)=rownames(NetGrowth)
   colnames(EachASV)=c("Slope","p-value","intercept")
   
   write.csv(EachASV, quote=F,file = paste("EachASV_Regression_", 
                                           CruiseStation[i],Treatment[i],"csv",sep="."))
-  }
+}
+
+
   
-  
-  
-  #### 2. Figure 2: Plot Competitiveness-Invulnerability trade-off ####
+##### C. Plot Figure 2: Plot Competitiveness-Invulnerability trade-off  
   HI_list_D02 = list.files(pattern="EachASV_Regression_.*.D02.csv") 
   HI_list_D30 = list.files(pattern="EachASV_Regression_.*.D30.csv") 
 
@@ -121,8 +120,7 @@ write.csv(NetGrowthRate_D30, quote=F,file = paste("NetGrowthRate_D30",
   par(oma = c(4, 4, 2, 2)) # make room for the overall x and y axis titles
   par(mar = c(0, 0, 0, 0)) # make the plots be closer together
   
-  ### A.Growth-based Competitiveness-Invulnerability trade-off ####
-  ## Protists-caused mortality (Slope_D02) ~ Predation-free growth rate (IGR) ####
+  ## Protists-caused mortality (Slope_D02) ~ Predation-free growth rate (IGR) 
   Xlim=c(-0.15,0.15)
   XlimLabels=c(-0.15,-0.1,-0.05,0,0.05,0.1,0.15)
   Ylim=c(-0.6,0.6)
@@ -166,7 +164,7 @@ write.csv(NetGrowthRate_D30, quote=F,file = paste("NetGrowthRate_D30",
   mtext("Viruses+protists",side=2, outer=T,cex=0.7, line=1.25, at=0.125)
   mtext("Top-down control-free growth rate",side=1, outer=T,cex=0.8, line=1)
   
-  #### Figure S6: Viral-caused mortality (SlopeD30-Slope_D02) ~ Predation-free growth rate (IGR) ####
+  ##### C. Plot Figure S6: Viral-caused mortality (SlopeD30-Slope_D02) ~ Predation-free growth rate (IGR) ####
   par(mfrow = c(2,3)) # 2-by-2 grid of plots
   par(oma = c(4, 4, 2, 2)) # make room for the overall x and y axis titles
   par(mar = c(0, 0, 0, 0)) # make the plots be closer together
@@ -187,16 +185,14 @@ write.csv(NetGrowthRate_D30, quote=F,file = paste("NetGrowthRate_D30",
   mtext("Resistance to viruses",side=2, outer=T,cex=0.8, line=1.5, at=0.5)
   mtext("Top-down control-free growth rate",side=1, outer=T,cex=0.8, line=1)
   
-#################################### End of Part 2 ########################################
+#################################### End of Part 1 ########################################
   
   
   
-######### Part 3: Preparing diversity index  ####
-  #### 1. Richness and Rank-based normalization #### 
+################# Part 2: Preparing diversity index  ############################
+  #### A. Richness and Rank-based normalization #### 
   # Rank-normalized ASV tables are for obtaining RAD and Evenness
-  library("gdata")
-  library("RADanalysis")
-  
+
   Table=ps.rarefied
   ### Finding minimum ranking among samples (smallest number of ASV among samples)
   TTable=as.data.frame(Table)
@@ -210,13 +206,13 @@ write.csv(NetGrowthRate_D30, quote=F,file = paste("NetGrowthRate_D30",
   write.table(nrad_mat,file="RankFixed_table.txt",quote=F)
   write.table(Richness,file="Richness.txt",quote=F)
   
-  #### 2. Calculate RAD decay coefficient and Evenness from rank-normalized RAD ####
+  #### B. Calculate RAD decay coefficient and Evenness from rank-normalized RAD ####
   library(vegan)
   library(reshape)
   library("lme4")
   library(nlme)
   library(viridis)
-  ### Calculate bacterial RAD and Evenness under protists and protists-viral effect ####
+  ### C. Calculate bacterial RAD and Evenness under protists and protists-viral effect ####
   ## Calculate RAD decay coefficient with rank with rank-normalized table
   gamma=zipfbrot.gamma(nrad_mat)
   gamma=gamma*(-1) # covert to positive value as decay coefficient
@@ -231,14 +227,36 @@ write.csv(NetGrowthRate_D30, quote=F,file = paste("NetGrowthRate_D30",
   
   write.table(finalTable,file="RankFixed_RAD_Evenness.txt",quote=F)
   
-#################################### End of Part 3 ########################################
+#################################### End of Part 2 ########################################
   
-########### Part 4: LMM analysis ################################################
+########### Part 3: LMM analysis ################################################
   
-  #### 1. Resistance ~ competition ####
+  #### A. Resistance ~ competition ####
+  temp = list.files(pattern="HypothesisI_variables.*.txt") 
+  l=list()
+  for (i in 1:length(temp)){
+    table=read.table(temp[i], header=T,row.names=1)
+    Sample=sapply(strsplit(temp,"_"), "[", 3)[i]
+    Station=sapply(strsplit(Sample,"St"), "[", 2)
+    l[[i]]=data.frame(table,rep(Sample,nrow(table)),rep(Station,nrow(table)))
+    colnames(l[[i]])=c(colnames(table),"Sample","Station")
+    names(l)[i]=Sample
+  }
+  rl=rbind(l[[1]],l[[2]],l[[3]],l[[4]],l[[5]],l[[6]],l[[7]])
+  
+  #Do LMM with function "LM_Estimates"
+  LMM_Estimates=rbind(LMMEst(rl$IGR,rl$Slope_D02,rl$Sample),
+                      LMMEst(rl$IGR,rl$Slope_D30-rl$Slope_D02,rl$Sample),
+                      LMMEst(log10(rl$T0_AbsA),rl$Slope_D02,rl$Sample),
+                      LMMEst(log10(rl$T0_AbsA),rl$Slope_D30-rl$Slope_D02,rl$Sample))
+  
+  colnames(LMM_Estimates)=c("Value" ,"Std. Error","DF","t-value","p-value","Fix_intercept","Fix_slope")     
+  rownames(LMM_Estimates)=c("Growth_Protists","Growth_Viruses",
+                            "Density_Protists","Density_Viruses")
+  write.csv(LMM_Estimates,"LMM_R-C.csv", quote=F) # Output LMM result for Hypothesis I
   
   
-  #### 2. Diversity ~ Top-down control dilution factors #### 
+  #### B. Diversity ~ Top-down control dilution factors #### 
   Dilution_WithT0=sapply(strsplit(names(Richness),"D"),"[",3)
   T0=which(is.na(Dilution_WithT0))
   NoT0_Richness=Richness[-T0]
@@ -308,14 +326,12 @@ write.csv(NetGrowthRate_D30, quote=F,file = paste("NetGrowthRate_D30",
   
   write.csv(LMM_Diversity,"LMM_Diversity.csv")
   
-  ##### Plotting #######
+#################################### End of Part 3 ########################################
   
 
-########### Part 5: Figure 3:Plot top-down control effect on diversity ################################################  
-  ############## Plot diversity ######
+########### Part 4: Plot Figure 3:Top-down control effect on diversity ################################################  
   COL=c("red","tomato2","blue","orange","springgreen4","chartreuse3","maroon","tan4","black")
   PCH=c(1,20,5,18,2,17,8,13)
-  ########################### PART 5: Plot Predation ~ Diversity relationship ################################################
   par(mfrow = c(3, 2)) # 1-by-3 grid of plots
   par(oma = c(3, 3, 2, 2)) # make room axis titles
   par(mar = c(1, 2, 0, 0)) # adjust width between plots 
@@ -323,7 +339,6 @@ write.csv(NetGrowthRate_D30, quote=F,file = paste("NetGrowthRate_D30",
   StationCruiseNames=c("2014AprSt1","2014OctSt1","2014OctSt9",
                        "2015JulSt1","2015JulSt9","2016MaySt1")
   
-  #### A. Figure 3: Plot predation ~ RAD decay coefficient, evenness, or richness relationships ####
   RDA_Ev_D02=split(EvennessRAD_D02,EvennessRAD_D02$CruiseStation)
   RDA_Ev_D30=split(EvennessRAD_D30,EvennessRAD_D30$CruiseStation)
   x=1 #gamma
@@ -393,9 +408,129 @@ write.csv(NetGrowthRate_D30, quote=F,file = paste("NetGrowthRate_D30",
          pch=PCH[1:6],col=COL[1:6],cex=0.6)
   mtext("Top-down control dilution factors", outer=T,side=1, line=1, cex=0.7)
   
-#################################### End of Part 3 ########################################
+#################################### End of Part 4 ########################################
   
+############ Part 5: Identify top-down control resistant and susceptible taxa #########################
+  
+  HI_list_D02 = list.files(pattern="EachASV_Regression_.*.D02.csv") 
+  HI_list_D30 = list.files(pattern="EachASV_Regression_.*.D30.csv") 
+  Taxonomy=read.table("taxonomy.tsv",header=T,sep="\t")
+  ASV_regressionWithTax_D02=list()
+  ASV_regressionWithTax_D30=list()
+  for (j in 1:6){
+    tax_D02=read.csv(HI_list_D02[j],header=T,row.names = 1)
+    tax_D30=read.csv(HI_list_D30[j],header=T,row.names = 1)
+    tax_all=data.frame(tax_D02$Slope,tax_D30$Slope,tax_D30$intercept,tax_D02$p.value,tax_D30$p.value)
+    rownames(tax_all)=rownames(tax_D02)
+    tax_all_D02=tax_all[tax_all$tax_D02.Slope!=0,]
+    SigOrNot_D02=tax_all_D02$tax_D02.p.value<=0.05
+    tax_all_D30=tax_all[tax_all$tax_D30.Slope!=0,]
+    SigOrNot_D30=tax_all_D30$tax_D30.p.value<=0.05
     
+    TT_D02=numeric()
+    TT_D30=numeric()
+    for (i in 1:length(rownames(tax_all_D02))){
+      TT_D02[i]=grep(rownames(tax_all_D02)[i], Taxonomy[,1], value = F)
+    }
+    Order_D02=sapply(strsplit(Taxonomy[TT_D02,]$Taxon,";"), "[", 4)
+    ASV_regressionWithTax_D02[[j]]=data.frame(tax_all_D02$tax_D02.Slope,
+                                              tax_all_D02$tax_D30.intercept,
+                                              Taxonomy[TT_D02,]$Feature.ID,
+                                              Order_D02,
+                                              SigOrNot_D02)
+    # if (length(rownames(Sig_D30)==0)){
+    
+    for (i in 1:length(rownames(tax_all_D30))){
+      TT_D30[i]=grep(rownames(tax_all_D30)[i], Taxonomy[,1], value = F) 
+      
+    } 
+    #}
+    Order_D30=sapply(strsplit(Taxonomy[TT_D30,]$Taxon,";"), "[", 4)
+    ASV_regressionWithTax_D30[[j]]=data.frame(tax_all_D30$tax_D30.Slope,
+                                              tax_all_D30$tax_D30.intercept,
+                                              Taxonomy[TT_D30,]$Feature.ID,
+                                              Order_D30,
+                                              SigOrNot_D30)
+  }
+  
+  names(ASV_regressionWithTax_D02)=sapply(strsplit(HI_list_D02, "\\_|\\.| "), "[", 4)
+  names(ASV_regressionWithTax_D30)=sapply(strsplit(HI_list_D30, "\\_|\\.| "), "[", 4)
+  
+  #########Plotting#####
+  M_D02=melt(ASV_regressionWithTax_D02,id=1:5)
+  Split_Order=sapply(strsplit(M_D02$Order, "__|;"), "[", 2)
+  ForPloting_D02=data.frame(M_D02[,1:2],Split_Order,M_D02[,5],M_D02[,6])
+  colnames(ForPloting_D02)=c("Slope","Intercept","Order","SigOrNot","Experiments")
+  
+  M_D30=melt(ASV_regressionWithTax_D30,id=1:5)
+  Split_Order=sapply(strsplit(M_D30$Order, "__|;"), "[", 2)
+  ForPloting_D30=data.frame(M_D30[,1:2],Split_Order,M_D30[,5],M_D30[,6])
+  colnames(ForPloting_D30)=c("Slope","Intercept","Order","SigOrNot","Experiments")
+  
+  
+  PlotFigure5_SigOnly=function(ForPloting_D02,Xlab){
+    library(dplyr)
+    
+    #Remove non-sig data
+    ForPloting_D02=filter(ForPloting_D02,ForPloting_D02$SigOrNot==T)
+    
+    ForPloting_D02$Order[which(is.na(ForPloting_D02$Order))]="uncultured"
+    ForPloting_D02=filter(ForPloting_D02,ForPloting_D02$Order!="marine metagenome",
+                          ForPloting_D02$Order!="unidentified marine bacterioplankton",
+                          ForPloting_D02$Order!="uncultured",
+                          ForPloting_D02$Order!="Unclassified",
+                          ForPloting_D02$Order!="unidentified marine eubacterium",
+                          ForPloting_D02$Order!="uncultured bacterium")
+    
+    
+    
+    NewOrderName=rep("a",length(ForPloting_D02$Order))
+    
+    for (i in 1:length(levels(as.factor(ForPloting_D02$Order)))){
+      O=levels(as.factor(ForPloting_D02$Order))[i]
+      OO=filter(ForPloting_D02,ForPloting_D02$Order==O)
+      OrderWithSig=sprintf("%s (%s/%s)",O,
+                           length(which(OO$SigOrNot==T & OO$Slope >= 0)),
+                           length(which(OO$SigOrNot==T & OO$Slope <= 0)))
+      NewOrderName[which(ForPloting_D02$Order==O)]=OrderWithSig
+    }
+    
+    ForPloting_D02=data.frame(ForPloting_D02,NewOrderName)
+    
+    #Rank Order based on resistance to predation
+    ForPloting_D02$NewOrderName = factor(ForPloting_D02$NewOrderName, 
+                                         levels=names(sort(tapply(ForPloting_D02$Slope,ForPloting_D02$NewOrderName, mean))))
+    
+    
+    ggplot(data=ForPloting_D02,aes(y=NewOrderName,x=Slope,horizontal=TRUE)) +
+      geom_boxplot(outlier.colour = NA) + theme_bw()+
+      geom_jitter(aes(),cex=1,shape=1,
+                  position=position_jitter(width = 0,height = 0)) + 
+      theme(axis.text=element_text(size=5.5))+
+      xlab(Xlab)+
+      ylab("")+ theme(legend.text=element_text(size=6),
+                      axis.title.x=element_text(size=7),
+                      axis.text.y=element_text(face="bold"))+
+      geom_vline(xintercept = 0, col="blue")
+    
+    
+    
+    
+    
+  }
+  
+  
+  pD02_tax=PlotFigure5_SigOnly(ForPloting_D02,"Resistance to protists")
+  
+  pD30_tax=PlotFigure5_SigOnly(ForPloting_D30,"Resistance to protists+viruses")
+  
+  ggarrange(pD02_tax,pD30_tax,nrow=1, widths = c(1, 1),
+            labels = c("A","B"),
+            font.label = list(size = 10),
+            legend="right", common.legend = TRUE) +
+    theme(plot.margin = margin(0.6,0.1,0.1,0.1, "cm"))
+  
+#################################### End of Part 4 ########################################
   
   
 ############ Part 6: supplementary table and figures #########################
@@ -426,8 +561,6 @@ write.csv(NetGrowthRate_D30, quote=F,file = paste("NetGrowthRate_D30",
   rownames(Final)=sapply(strsplit(temp, "_", fixed = TRUE), "[", 3)
   write.csv(Final,"Permutation_shapiro_Protists+viruses-IGR_RegresionSlope.csv")
 
-############## Figure S1: Map #####    
-  
 ##########################################################################################
   
 ############## Figure S3: Total abundance at T0 and T12 plot #####
